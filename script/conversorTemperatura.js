b1e509c8f6919089e499e0497b28a4ad26c72ff8
Scharles Souza <souza.scharles@gmail.com>
2020-08-28 22:16:26 -0300

    Primeiro commit da pagina

---
+++ script/conversorTemperatura.js
@@ -0,0 +1,25 @@
+const fahrenheit = document.getElementById('fahrenheit');
+const kelvin = 	document.getElementById('kelvin');
+const celsius = document.getElementById('celsius');
+
+
+ function fromCelsius(){
+	var valor = celsius.value;
+	fahrenheit.value = (1.8*parseFloat(valor)+32).toFixed(2);
+	kelvin.value = (parseFloat(valor)+273).toFixed(2);
+}
+function fromFahrenheit(){
+	var valor =  fahrenheit.value;
+	celsius.value = ((parseFloat(valor)-32)/1.8).toFixed(2);
+	kelvin.value = ((parseFloat(valor)-32)*(5/9)+273.15).toFixed(2);
+}
+function fromKelvin(){
+	var valor= kelvin.value;
+	celsius.value = (parseFloat(valor) - 273).toFixed(2);
+	fahrenheit.value = ((parseFloat(valor)-273)*(9/5)+32).toFixed(2);
+}
+function limparCampos(){
+	fahrenheit.value = "";
+	kelvin.value = "";
+	celsius.value = "";
+}
