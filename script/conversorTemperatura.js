cdb2771047a7a3840dac3a02d28a36ec0868fe64
Scharles Souza <souza.scharles@gmail.com>
2020-08-28 23:27:23 -0300

    atualização no JS

--- script/conversorTemperatura.js
+++ script/conversorTemperatura.js
@@ -6,16 +6,16 @@ const celsius = document.getElementById('celsius');
  function fromCelsius(){
 	var valor = celsius.value;
 	fahrenheit.value = (1.8*parseFloat(valor)+32).toFixed(2);
-	kelvin.value = (parseFloat(valor)+273).toFixed(2);
+	kelvin.value = (parseFloat(valor)+273);
 }
 function fromFahrenheit(){
 	var valor =  fahrenheit.value;
-	celsius.value = ((parseFloat(valor)-32)/1.8).toFixed(2);
-	kelvin.value = ((parseFloat(valor)-32)*(5/9)+273).toFixed(2);
+	celsius.value = ((parseFloat(valor)-32)/1.8);
+	kelvin.value = ((parseFloat(valor)-32)*(5/9)+273);
 }
 function fromKelvin(){
 	var valor= kelvin.value;
-	celsius.value = (parseFloat(valor) - 273).toFixed(2);
+	celsius.value = (parseFloat(valor) - 273);
 	fahrenheit.value = ((parseFloat(valor)-273)*(9/5)+32).toFixed(2);
 }
 function limparCampos(){
